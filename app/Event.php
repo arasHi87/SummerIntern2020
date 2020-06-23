9daf6381a5eafd537127cb8a2dbaee116078e88e
arashi87 <arasi27676271@gmail.com>
2020-06-24 03:34:11 +0800

    update fillable notice_day, notice_dat_type

--- app/Event.php
+++ app/Event.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-        'title', 'start_time', 'end_time', 'user_id', 'bg_color', 'text_color',
+        'title', 'start_time', 'end_time', 'user_id', 'bg_color', 'text_color', 'notice_day', 'notice_day_type',
     ];
 }
